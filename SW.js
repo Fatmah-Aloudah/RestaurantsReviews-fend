0a50d82e8304f42317dc5745213ce865806db921
Fatmah Sami Al-Oudah <Fatmah.Al.Oudah@gmail.com>
2019-12-23 15:30:26 +0300

    Update SW.js

--- SW.js
+++ SW.js
@@ -54,14 +54,26 @@ self.addEventListener('install', function(event) {
 });
 
 
-self.addEventListener('fetch', function(event) {
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then(function(response) {
+//         // Cache hit - return response
+//         if (response) {
+//           return response;
+//         }
+        
+  self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.match(event.request)
+          .then((response) => {
+            return response || fetch(event.request)
+          });
+      })
+  );
+});
 
         return fetch(event.request).then(
           function(response) {
