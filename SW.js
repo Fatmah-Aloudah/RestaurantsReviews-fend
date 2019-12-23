4c189b4484cba0d240c7d6343f61ffc49028c7d2
Fatmah Sami Al-Oudah <Fatmah.Al.Oudah@gmail.com>
2019-12-23 15:23:24 +0300

    Update SW.js

--- SW.js
+++ SW.js
@@ -1,30 +1,30 @@
 var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
   '/',
-  './index.html',
-       './index.html?homescreen=1',
-       './?homescreen=1',
-       './resturants.html',
-       './css/styles.css',
-       './data/resturants.json',
-       './js/dbhelper.js',
-       './js/main.js',
-       './js/resturant_info.js',
-       './img/1.jpg',
-       './img/2.jpg',
-       './img/3.jpg',
-       './img/4.jpg',
-       './img/5.jpg',
-       './img/6.jpg',
-       './img/7.jpg',
-       './img/8.jpg',
-       './img/9.jpg',
-       './img/10.jpg'
+  '/index.html',
+       '/index.html?homescreen=1',
+       '/?homescreen=1',
+       '/resturants.html',
+       '/css/styles.css',
+       '/data/resturants.json',
+       '/js/dbhelper.js',
+       '/js/main.js',
+       '/js/resturant_info.js',
+       '/img/1.jpg',
+       '/img/2.jpg',
+       '/img/3.jpg',
+       '/img/4.jpg',
+       '/img/5.jpg',
+       '/img/6.jpg',
+       '/img/7.jpg',
+       '/img/8.jpg',
+       '/img/9.jpg',
+       '/img/10.jpg'
 ];
 
 IndexController.prototype._registerServiceWorker = function(){
 if (!navigator.serviceWorker) return;
-navigator.serviceWorker.register('/SW.js').then(function(){
+navigator.serviceWorker.register('./SW.js').then(function(){
 console.log('Registration worked!');
 }).catch(function(){
 console.log('Registration failed!');
