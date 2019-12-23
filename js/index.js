2f905158ffe79f984816b5ace3a5d548644f1d5f
Fatmah Sami Al-Oudah <Fatmah.Al.Oudah@gmail.com>
2019-12-23 22:55:23 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,29 +1,6 @@
-// if (navigator.serviceWorker) {
-//   navigator.serviceWorker.register('./SW.js')
-//     .then(() => {
-//     	scope: '/Resturants/'
-//       console.log('ServiceWorker registration successful');
-//     })
-//     .catch(err => {
-//       console.log('ServiceWorker registration failed', err);
-//     })
-// }
-
-// if ('serviceWorker' in navigator){
-//   navigator.serviceWorker.register('./SW.js').then(function(registration){
-//     console.log('service worker registration succeeded:',registration);
-//   },
-// function(error){
-//   console.log('service worker registration failed:',error);
-// });
-// }
-// else{
-//   console.log('service workers are not supported.');
-// }
-
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/SW.js').then(function(registration) {
+    navigator.serviceWorker.register('./SW.js').then(function(registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
     }, function(err) {
